0f80f530f4a7304627077c556ce6c2fdfd3a0eda
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-21 12:53:34 +0200

    commit use cors

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ require('dotenv').config()
 
 const app = express();
 app.use(express.json());
-app.use(cors({
+/*app.use(cors({
   origin:["http://localhost:5173"],
   methods:["POST","GET"],
   allowedHeaders:"Content-Type",
   credentials:true
-}));
+}));*/
+
+app.use(cors())
 
 const apiKey = process.env.OPENAI_API_KEY;
 const apiUrl = "https://api.openai.com/v1/chat/completions";
